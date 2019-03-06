bff123ca7f737a59716a03088ac8e5ebffadf1a2
BuildTools <unconfigured@null.spigotmc.org>
2019-03-06 18:21:22 +0900

    create a class 'RunManager', and add a little changes in MCBS.java.

--- src/main/java/net/iegmann/mcbs/mcbs/MCBS.java
+++ src/main/java/net/iegmann/mcbs/mcbs/MCBS.java
@@ -1,7 +1,13 @@
 package net.iegmann.mcbs.mcbs;
 
+import net.iegmann.mcbs.mcbs.commands.RunManager;
+import net.iegmann.mcbs.mcbs.managers.GameManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * クラスMCBSは本プラグインのメインクラスです.
+ * ここではプラグインそのものの管理に関する処理（ex.コマンド登録）をします.
+ */
 public final class MCBS extends JavaPlugin {
     public static MCBS plugin;
     public static MCBS getInstance(){
@@ -11,6 +17,8 @@ public final class MCBS extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
+        GameManager.getInstance();
+        this.getCommand("run").setExecutor(new RunManager());
     }
 
     @Override
